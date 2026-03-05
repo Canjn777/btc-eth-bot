fe9bb5b52513686d934b26265ac57c27bca52fa7
Canjn777 <zhaochuanbo777@gmail.com>
2026-03-05 17:13:39 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,73 +1,115 @@
 import os
 import time
-import requests
 import pandas as pd
 from binance.client import Client
 import ta
 from telegram import Bot
+
 print("BOT STARTED")
+
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 bot = Bot(token=TELEGRAM_TOKEN)
+
+bot.send_message(chat_id=CHAT_ID, text="🤖 Trading Bot is online")
 
 client = Client()
 
 symbols = ["BTCUSDT", "ETHUSDT"]
 
 def get_data(symbol):
-    klines = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_5MINUTE, limit=100)
+    klines = client.get_klines(
+        symbol=symbol,
+        interval=Client.KLINE_INTERVAL_5MINUTE,
+        limit=200
+    )
+
     df = pd.DataFrame(klines, columns=[
         "time","open","high","low","close","volume",
         "close_time","qav","num_trades","taker_base_vol",
         "taker_quote_vol","ignore"
     ])
+
     df["close"] = df["close"].astype(float)
     return df
+
 
 def check_signal(symbol):
 
     df = get_data(symbol)
 
-    rsi = ta.momentum.RSIIndicator(df["close"]).rsi().iloc[-1]
     price = df["close"].iloc[-1]
 
-    if rsi < 30:
+    rsi = ta.momentum.RSIIndicator(df["close"]).rsi().iloc[-1]
+
+    macd = ta.trend.MACD(df["close"]).macd_diff().iloc[-1]
+
+    ema20 = ta.trend.EMAIndicator(df["close"], window=20).ema_indicator().iloc[-1]
+    ema50 = ta.trend.EMAIndicator(df["close"], window=50).ema_indicator().iloc[-1]
+
+    # LONG SIGNAL
+    if rsi < 35 and macd > 0 and ema20 > ema50:
+
+        tp1 = price * 1.01
+        tp2 = price * 1.02
+        tp3 = price * 1.04
+        sl = price * 0.985
+
         message = f"""
-🚨 BUY SIGNAL
+🚀 LONG SIGNAL
 
 Coin: {symbol}
-Price: {price}
-RSI: {round(rsi,2)}
+Entry: {round(price,2)}
 
-Oversold market
-Possible bounce
+TP1: {round(tp1,2)}
+TP2: {round(tp2,2)}
+TP3: {round(tp3,2)}
+
+SL: {round(sl,2)}
+
+RSI: {round(rsi,2)}
 """
+
         bot.send_message(chat_id=CHAT_ID, text=message)
 
-    if rsi > 70:
+    # SHORT SIGNAL
+    if rsi > 65 and macd < 0 and ema20 < ema50:
+
+        tp1 = price * 0.99
+        tp2 = price * 0.98
+        tp3 = price * 0.96
+        sl = price * 1.015
+
         message = f"""
-⚠️ SELL SIGNAL
+🔻 SHORT SIGNAL
 
 Coin: {symbol}
-Price: {price}
-RSI: {round(rsi,2)}
+Entry: {round(price,2)}
 
-Overbought market
-Possible pullback
+TP1: {round(tp1,2)}
+TP2: {round(tp2,2)}
+TP3: {round(tp3,2)}
+
+SL: {round(sl,2)}
+
+RSI: {round(rsi,2)}
 """
+
         bot.send_message(chat_id=CHAT_ID, text=message)
+
 
 while True:
 
     try:
 
         for symbol in symbols:
+
             check_signal(symbol)
 
         time.sleep(300)
 
     except Exception as e:
+
         print(e)
         time.sleep(60)
-
